ed39cf6d11684173b24e0f1e7f471510f0ca511d
tracer-nju <tracer@nju.edu.cn>
2024-11-19 03:23:33 +0800

    Linux DESKTOP-9QNMAV2 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:23:33 up 2 days,  6:50,  1 user,  load average: 0.05, 0.18, 0.19

--- libco/co.c
+++ libco/co.c
@@ -137,7 +137,7 @@ struct co {
   void (*func)(void *); // co_start 指定的入口地址和参数
   void *arg;
 
-  size_t call_cnt;
+  __attribute__((aligned(16))) size_t call_cnt;
   enum co_status status;  // 协程的状态
   struct co *    waiter;  // 是否有其他协程在等待当前协程
   jmp_buf        context; // 寄存器现场 (setjmp.h)
