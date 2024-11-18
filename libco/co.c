fa49df8732ec20d97e51991e0da684fe28c3ff40
tracer-nju <tracer@nju.edu.cn>
2024-11-19 04:19:52 +0800

    Linux DESKTOP-9QNMAV2 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:19:52 up 2 days,  7:47,  1 user,  load average: 0.04, 0.09, 0.08

--- libco/co.c
+++ libco/co.c
@@ -242,9 +242,9 @@ void co_yield() {
   size_t least_called_val = SIZE_MAX;
   list_for_each_entry(exec_co, &coroutine_list, co_list) {
     printf("%s at %p is at status: %s\n", exec_co->name, exec_co, status_map[exec_co->status]);
-    // if (exec_co == curr_co) {
-    //   continue;
-    // }
+    if (exec_co == curr_co) {
+      continue;
+    }
     if (exec_co->status == CO_NEW
         || exec_co->status == CO_RUNNABLE
         || exec_co->status == CO_WAITING) {
