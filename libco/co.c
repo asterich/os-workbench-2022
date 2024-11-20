a2487baed40883b5cad25c2fa3aff4be1f067dd0
tracer-nju <tracer@nju.edu.cn>
2024-11-20 21:15:55 +0800

    Linux DESKTOP-9QNMAV2 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:15:55 up 4 days, 43 min,  1 user,  load average: 0.02, 0.11, 0.09

--- libco/co.c
+++ libco/co.c
@@ -299,9 +299,9 @@ void co_yield() {
       /// Set status to CO_DEAD.
       // printf("coroutine %s is dead.\n", exec_co->name);
       exec_co->status = CO_DEAD;
-      curr_co = exec_co->waiter;
-      longjmp(curr_co->context, 1);
-      // co_yield();
+      // curr_co = exec_co->waiter;
+      // longjmp(curr_co->context, 1);
+      co_yield();
     }
     break;
 
